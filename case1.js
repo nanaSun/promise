d58a934598dd3b79d65a2dcab90a90d63bab3bea
nanaSun <1128yeye@sina.com>
2018-08-03 17:41:27 +0800

    promise

--- case1.js
+++ case1.js
@@ -1,9 +1,28 @@
-let promise=require("./promise1")
-let promiseCase=new promise((resolve,reject)=>{
-    //此处自行定义
+let Promise=require("./promise1")
+let fs=require("fs")
+
+function newPromise(){
+    return new Promise((resolve,reject)=>{
+        resolve("bbbbbbb");
+            // fs.readFile('./promise/1.txt','utf-8',(err,data)=>{
+            //     if(err){
+            //         reject(err)
+            //     }else{
+            //         resolve(data)
+            //     }
+            // })
+    })
+}
+
+let i=0;
+let b=newPromise().then((value)=>{
+    console.log(value)
+    return new Promise((resolve,reject)=>{
+        resolve(new Promise((resolve,reject)=>{
+            resolve("aaddddaa")
+        }))
+    })
 })
-promiseCase.then((value)=>{
-
-},(reason)=>{
-
+b.then((value)=>{
+    console.log(value)
 })
